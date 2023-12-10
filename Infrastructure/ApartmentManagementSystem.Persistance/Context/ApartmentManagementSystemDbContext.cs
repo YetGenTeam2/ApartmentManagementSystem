4e2e5926de5ccb9931edaec39243e5389908107d
Celal Karahan <celal51kara@gmail.com>
2023-12-10 21:53:05 +0300

    [Feat] project created
    The project was created and the necessary files for the identity mechanism started to be created.

---
+++ Infrastructure/ApartmentManagementSystem.Persistance/Context/ApartmentManagementSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApartmentManagementSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace ApartmentManagementSystem.Persistance.Context
+{
+    public class ApartmentManagementSystemDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public ApartmentManagementSystemDbContext(DbContextOptions<ApartmentManagementSystemDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL"));
+        }
+    }
+}
