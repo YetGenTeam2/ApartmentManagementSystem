de78901020b34c4edf75a201fbac0c28503ef166
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-12-17 00:29:17 +0300

    toaster service added

---
+++ Presentation/ApartmentManagementSystem.MVC/Services/IToasterService.cs
@@ -0,0 +1,12 @@
+﻿namespace ApartmentManagementSystem.MVC.Services
+{
+    public interface IToasterService 
+    {
+        void ShowSuccess(string message);
+
+        void ShowInfo(string message);
+
+        void ShowWarning(string message);
+        void ShowError(string message);
+    }
+}
---
+++ Presentation/ApartmentManagementSystem.MVC/Services/SotseraToastService.cs
@@ -0,0 +1,34 @@
+﻿using Sotsera.Blazor.Toaster;
+
+namespace ApartmentManagementSystem.MVC.Services
+{
+    public class SotseraToastService : IToasterService
+    {
+        private readonly IToaster _toaster;
+
+        public SotseraToastService(IToaster toaster)
+        {
+            _toaster = toaster;
+        }
+
+        public void ShowError(string message)
+        {
+            _toaster.Error(message);
+        }
+
+        public void ShowInfo(string message)
+        {
+            _toaster.Info(message);
+        }
+
+        public void ShowSuccess(string message)
+        {
+            _toaster.Success(message);
+        }
+
+        public void ShowWarning(string message)
+        {
+            _toaster.Warning(message);
+        }
+    }
+}
