7e9294ab6015515091794d8888d0238fd591dada
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-12-17 00:28:44 +0300

    subscription fee adding page&controller

---
+++ Presentation/ApartmentManagementSystem.MVC/Areas/Admin/Controllers/SubscriptionController.cs
@@ -0,0 +1,55 @@
+﻿using ApartmentManagementSystem.Domain.Entities;
+using ApartmentManagementSystem.Persistance.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentManagementSystem.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class SubscriptionController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ApartmentManagementSystemDbContext _context;
+
+        public SubscriptionController(UserManager<AppUser> userManager, ApartmentManagementSystemDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Daire> daireList = _context.daires.ToList();
+            return View(daireList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubscriptionEntry(decimal price)
+        {
+            List<Daire> daireList =  _context.daires.ToList();
+            List<Subscription> subscriptions = new List<Subscription>();
+
+            foreach(Daire daire in daireList)
+            {
+                Subscription subscription = new Subscription();
+
+                subscription.price = price;
+                subscription.daire = daire;
+                subscription.isPaid = false;
+                subscription.ModifiedOn = DateTime.Now;
+
+                subscriptions.Add(subscription);
+
+            }
+
+            await _context.subscriptions.AddRangeAsync(subscriptions);
+            await _context.SaveChangesAsync();
+
+            return View();
+        }
+    }
+}
