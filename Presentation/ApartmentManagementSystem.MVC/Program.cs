dc697cdf137639c92ce9b4e5938eeb78ffe3dd36
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-12-17 00:29:17 +0300

    toaster service added

--- Presentation/ApartmentManagementSystem.MVC/Program.cs
+++ Presentation/ApartmentManagementSystem.MVC/Program.cs
@@ -5,12 +5,24 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Resend;
 using ApartmentManagementSystem.MVC;
+using ApartmentManagementSystem.MVC.Services;
+using Sotsera.Blazor.Toaster.Core.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // ConfigureServices metodu için Startup sýnýfýný kullan
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
+
+builder.Services.AddScoped<IToasterService, SotseraToastService>();
+
+builder.Services.AddToaster(config =>
+{
+    //example customizations
+    config.PositionClass = Defaults.Classes.Position.TopRight;
+    config.PreventDuplicates = true;
+    config.NewestOnTop = false;
+});
 
 var app = builder.Build();
 
