9c43efd52fecdc5165012e99e3520bf02f67b1f1
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-21 09:55:53 +0300

    [Update]: Update  Daires Controller And View

--- Presentation/ApartmentManagementSystem.MVC/Areas/Admin/Controllers/UpdateDaireController.cs
+++ Presentation/ApartmentManagementSystem.MVC/Areas/Admin/Controllers/UpdateDaireController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApartmentManagementSystem.MVC.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class UpdateDaireController : Controller
     {
-        public IActionResult Index()
+        [HttpPost]
+        public IActionResult UpdateDaire()
         {
             return View();
         }
